a03a7d628d919b730c97a83c5cb9b038f81980a7
diego20232 <diegosampaio986@gmail.com>
2024-02-27 11:46:07 -0300

    projetoReact

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View } from "react-native"; 
+
+import Parte, {Parte1,Parte2 } from "./components/primeiro";
+
+function App() {
+    return (
+        <View>
+        <Parte/>
+        <Parte1/>
+        </View>
+    ) 
+}
+
+export default App
+
